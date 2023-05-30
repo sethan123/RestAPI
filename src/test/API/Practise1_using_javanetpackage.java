73c23b3880d6c3174f792ab084ed4eecbc3bf102
setha <sethan15@hotmail.com>
2023-05-30 23:12:31 +0530

    Rest API using java.net package

--- src/test/API/Practise1_using_javanetpackage.java
+++ src/test/API/Practise1_using_javanetpackage.java
@@ -1,11 +1,10 @@
-import org.apache.http.HttpConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-public class Practise1 {
+public class Practise1_using_javanetpackage {
         public static void main(String[] args) {
             String apiUrl = "https://reqres.in/api/users?page=2";
 
